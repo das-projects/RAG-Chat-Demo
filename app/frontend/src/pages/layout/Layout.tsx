eead13fcdf2165d629ddacc3a9055d9ffa4c0ca9
Arijit Das <arijitd@gmail.com>
2023-11-24 17:02:06 +0100

    edit layout

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>Nexible Chat Bot</h3>
+                        <h3 className={styles.headerTitle}>Nexible Chatbot</h3>
                     </Link>
                     {/*
                     <nav>
